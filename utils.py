abd477412ca5cb1963ce6ec7019d8ba39b16ef4e
Ujjwal Sharma <mail@ujjwalsharma.net>
2020-04-26 13:25:04 +0000

    Add code for readable string representations for time diffs

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+import pytz
 import yaml
 
 
@@ -16,3 +19,25 @@ def load_config(path):
     except FileNotFoundError:
         print("Missing YAML configuration at '%s'" % (path))
         exit(0)
+
+
+def strf_time_diff(t1, t2):
+    diff = t1 - t2
+    div = divmod(diff.seconds, 3600)
+    hours = div[0]
+
+    if hours > 0:
+        return f"{hours} hour(s)"
+
+    div = divmod(div[1], 60)
+    minutes = div[0]
+    seconds = div[1]
+
+    if minutes > 0:
+        return f"{minutes} minute(s)"
+    else:
+        return f"{seconds} second(s)"
+
+
+def date_now():
+    datetime.today().date()
