dfd7bf0ee7223edffd2aea503f64ee95a9140fe8
Ujjwal Sharma <mail@ujjwalsharma.net>
2020-04-26 13:25:45 +0000

    This commit lands the following modifications:
    1. StatusManager now generates a events log page. This may be necessary for checking container start and stop history.
    2. Simplifications to the code to better use class methods and reduce complexity.
    3. General cleanup and fixes.

--- StatusManager.py
+++ StatusManager.py
@@ -1,44 +1,84 @@
+import copy
 import os
 import sys
 import time
+from datetime import datetime
 
 import docker
 
-from Parse import publish_status_page
+from Publish import hugo_build, publish_event_page, publish_status_page
 from utils import load_config
 
 
 class StatusManager:
     def __init__(self, config):
         self.client = docker.from_env()
-        self.poll_interval = config["poll_interval"]
+        self.poll_interval = int(config["poll_interval"])
         self.website_src_path = config["website_src_path"]
+        self.prev_running_containers = set()
+        self.running_containers = set()
+        self.logs = []
+
+        self.mandatory_rebuild_time_diff = (
+            int(config["mandatory_rebuild_time_diff"]) * 60
+        )
 
         # Start Monitoring.
         self._monitor()
 
     def _monitor(self):
-        prev_running_containers = set()
         try:
             while True:
-                running_containers = set(self.client.containers.list())
-                if running_containers != prev_running_containers:
-                    self._publish_status_page(running_containers)
-                    prev_running_containers = running_containers
+                self.running_containers = set(self.client.containers.list())
+
+                if self.running_containers != self.prev_running_containers:
+                    self._publish_status_page()
+                    self._publish_event_page()
+                    self._rebuild_website()
+
+                    self.prev_running_containers = self.running_containers
+                    last_updated_timestamp = datetime.now()
 
                 time.sleep(self.poll_interval)
+
+                time_diff = datetime.now() - last_updated_timestamp
+                if time_diff.seconds > self.mandatory_rebuild_time_diff:
+                    self._publish_status_page()
+                    self._publish_event_page()
+                    self._rebuild_website()
+
+                    last_updated_timestamp = datetime.now()
 
         except KeyboardInterrupt:
             print("Exiting ...")
             sys.exit(0)
 
-    def _publish_status_page(self, status):
-        publish_status_page(status, website_src_path=self.website_src_path)
+    def _publish_status_page(self):
+        publish_status_page(
+            self.running_containers, website_src_path=self.website_src_path
+        )
 
-    # def _generate_container(self):
-    #     return set([c.attrs["Config"]["Image"]: c.attrs["State"]["Status"]
-    #         for c in self.client.containers.list()
-    #     }
+    def _publish_event_page(self):
+        self.logs = self._generate_logs()
+        publish_event_page(
+            self.logs, website_src_path=self.website_src_path,
+        )
+
+    def _rebuild_website(self):
+        # Run Hugo Build.
+        hugo_build(self.website_src_path)
+
+    def _generate_logs(self):
+        logs = self.logs
+        started_containers = [
+            (container, datetime.now(), "started")
+            for container in self.running_containers - self.prev_running_containers
+        ]
+        destroyed_containers = [
+            (container, datetime.now(), "destroyed")
+            for container in self.prev_running_containers - self.running_containers
+        ]
+        return started_containers + destroyed_containers + logs
 
 
 if __name__ == "__main__":
